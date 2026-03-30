ac4eaf04157fdc04118683ea99ec2df43b6a5e5e
VTCodeCraft <work.vishesh12.05@gmail.com>
2026-03-30 23:28:36 +0530

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Geist } from "next/font/google";
 import "./globals.css";
-import AuroraBackground from "@/components/aurora-background";
 import Header from "@/components/header";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import { cn } from "@/lib/utils";
@@ -28,8 +27,10 @@ export default function RootLayout({
         className={`${inter.className} bg-[#f7f7fb] text-gray-900 relative min-h-screen overflow-x-hidden`}
         suppressHydrationWarning
       >
-        <AuroraBackground />
-
+        {/* BACKGROUND BLOBS */}
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        
         <ActiveSectionContextProvider>
         <Header />
         {/* CONTENT */}
