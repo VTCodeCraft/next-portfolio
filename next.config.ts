f4bb47ec540d89934b7158c567d5091153d8e42c
VTCodeCraft <work.vishesh12.05@gmail.com>
2026-05-13 18:28:25 +0530

    feat: add redirect configuration for vtcodecraft.in and update Open Graph image

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,21 @@ const nextConfig: NextConfig = {
   images: {
     qualities: [25, 75, 95],
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "vtcodecraft.in",
+          },
+        ],
+        destination: "https://www.vtcodecraft.in/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
